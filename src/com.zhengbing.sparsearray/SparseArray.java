d4fc786caaad944195a78d238728f15966def510
zhengbing <mydreambing@126.com>
2020-07-14 01:14:35 +0800

    sparsearray

---
+++ src/com.zhengbing.sparsearray/SparseArray.java
@@ -0,0 +1,26 @@
+public class SparseArray{
+
+    public static void main(String[] args){
+        // 1. 创建一个原始的二维数组 11*11
+        // 0 表示没有妻子， 1表示黑棋  2 表示蓝棋
+        int chessArray[][] = new int[11][11];
+
+        chessArray[1][2] = 1;
+        chessArray[2][3] = 2;
+
+        //  输出原始数组
+
+        // 2. 将二维数组转为稀疏数组，
+
+        // 先遍历原始数组，得到非0数据的个数
+
+        // 创建对应的稀疏数组
+
+        // 给对应的稀疏数组赋值
+
+        // 遍历原始二维数组，将非0数据 存放到  sparseArray
+
+
+        // 3 输出稀疏数组的形式
+    }
+}
